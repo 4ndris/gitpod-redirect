db6f59c00aaeb2bb79733e8e3149346f1db25b23
Colton Fischer <coltonfischer@users.noreply.github.com>
2019-07-16 20:29:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
 
-    return "Hello World!"
+    url = "https://github.com/coltonfischer/gitpod-redirect"
+
+    if request.referrer and host(request.referrer) == "github.com":
+        url = "https://gitpod.io/#" + request.referrer
+
+    return redirect(url)
+
+def host(url):
+    
+    spltAr = url.split("://")
+    i = (0,1)[len(spltAr)>1]
+    return spltAr[i].split("?")[0].split('/')[0].split(':')[0].lower()
 
 
 if __name__ == '__main__':
